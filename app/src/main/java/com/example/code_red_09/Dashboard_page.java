9dcc4d434c1b28c87557971dab31bb68fb17fe5a
rmjay <rm.jayanthan05@gmail.com>
2023-11-03 02:21:20 +0530

    Dashboard

---
+++ app/src/main/java/com/example/code_red_09/Dashboard_page.java
@@ -0,0 +1,14 @@
+package com.example.code_red_09;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboard_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_page);
+    }
+}
